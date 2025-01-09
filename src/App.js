208ff926b4c7d38e925d3338ad0dd7a4e81a2c49
ks <146908390+ks415@users.noreply.github.com>
2025-01-09 16:27:42 +0900

    pokemonの詳細データを取得

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllpokemon } from './utils/pokemon';
+import { getAllpokemon, getPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       //全てのポケモンデータを取得
       let res = await getAllpokemon(initialURL);
-      console.log(res);
+      //ポケモンの詳細データを取得
+      loadPokemon(res.results);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async(data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+  };
+  
   return (
     <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました</h1>
+      )}
     </div>
   );
 }
